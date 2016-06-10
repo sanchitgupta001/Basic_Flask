968cfada28a8780144ac20a96c9b2515079c5ab5
sanchitgupta001 <sanchit.gupta011@gmail.com>
2016-06-11 00:37:02 +0530

    Error handling

--- app/content_management.py
+++ app/content_management.py
@@ -1,6 +1,7 @@
 def Content():
+    # Dictionary
     TOPIC_DICT = {"Basics": [["url1","/dashboard1/"],
                              ["url2","/dashboard2/"],
                              ["url3","/dashboard3/"]],
                   "Web-Dev": []}
-    return TOPIC_DICT              
+    return TOPIC_DICT
