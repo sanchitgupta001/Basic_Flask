95d3fc81eb04a4bcac32868cd97e541abdad607c
sanchitgupta001 <sanchit.gupta011@gmail.com>
2016-06-10 23:58:08 +0530

    Add content_management.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from content_management import Content
+
+TOPIC_DICT = Content()
 
 app = Flask(__name__)
 
@@ -8,7 +11,7 @@ def homepage():
 
 @app.route('/dashboard/')
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("dashboard.html",TOPIC_DICT = TOPIC_DICT)
 
 if __name__=="__main__":
     app.run()
---
+++ app/content_management.py
@@ -0,0 +1,6 @@
+def Content():
+    TOPIC_DICT = {"Basics": [["url1","/dashboard1/"],
+                             ["url2","/dashboard2/"],
+                             ["url3","/dashboard3/"]],
+                  "Web-Dev": []}
+    return TOPIC_DICT              
